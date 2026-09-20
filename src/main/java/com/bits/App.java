26a19a0a965340a493ff0901ed976bb6caa3676d
sreesankarm <2022wb86366@wilp.bits-pilani.ac.in>
2026-09-20 20:30:44 +0530

    Update App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps! Student ID: 2022WB86366 - Version 1");
+        System.out.println("Hello DevOps! Student ID: 2022WB86366 - Version 2 (Modified for Continuous Integration)");
     }
 }
